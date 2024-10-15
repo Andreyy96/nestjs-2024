4a5b375068ed7c02235094c6e5f740eb4ff5f5c9
Andreyy96 <taurussilver777@gmail.com>
2024-10-15 13:01:35 +0300

    lesson1 swagger, husky, eslintrc

--- src/users/users-admin.controller.ts
+++ src/users/users-admin.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
 import { UsersAdminService } from './users-admin.service';
 
+@ApiTags('Users-Admin')
 @Controller('users-admin')
 export class UsersAdminController {
   constructor(private readonly usersService: UsersAdminService) {}
